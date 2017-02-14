e256227270371a64e626d5e747d9e122eabb2405
Elena Esina <hellemess@yandex.ru>
2017-02-14 15:54:31 +0300

    добавила модуль, который управляет поведением меток на карте, и модуль, который синхронизирует значения в полях формы

---
+++ js/initialize-pins.js
@@ -0,0 +1,53 @@
+'use strict';
+
+window.initializePins = function () {
+  var clearPins = function () {
+    for (i = 0; i < window.pin.length; i++) {
+      window.pin[i].classList.remove('pin--active');
+      window.pin[i].setAttribute('aria-pressed', false);
+    }
+  };
+
+  var selectPin = function (target) {
+    target.classList.add('pin--active');
+    target.setAttribute('aria-pressed', true);
+    window.dialog.style.display = 'block';
+    window.dialogClose.focus();
+  };
+
+  var closeDialog = function (evt) {
+    evt.preventDefault();
+    window.dialog.style.display = 'none';
+    var activePin = window.pinMap.querySelector('.pin--active');
+    activePin.classList.remove('pin--active');
+  };
+
+  window.pinMap.addEventListener('click', function (evt) {
+    if (evt.path < 8) {
+      return;
+    }
+    clearPins();
+    if (evt.target.tagName === 'div') {
+      var selectedPin = evt.target;
+    } else {
+      selectedPin = evt.path[1];
+    }
+    selectPin(selectedPin);
+  });
+
+  window.pinMap.addEventListener('keydown', function (evt) {
+    if (isActivateEvent(evt)) {
+      clearPins();
+      var selectedPin = evt.target;
+      selectPin(selectedPin);
+    }
+  });
+
+  window.dialogClose.addEventListener('click', closeDialog);
+
+  window.dialogClose.addEventListener('keydown', function (evt) {
+    if (isActivateEvent(evt)) {
+      closeDialog(evt);
+    }
+  });
+};
---
+++ js/synchronize-fields.js
@@ -0,0 +1,15 @@
+'use strict';
+
+window.synchronizeFields = function (selectedField, dependentField, selectedArray, dependentArray, dependentAttribute) {
+  for (i = 0; i < Math.max(selectedArray.length, dependentArray.length); i++) {
+    if (!!selectedField[i]) {
+      selectedField[i].value = selectedArray[i];
+    }
+    if (!!dependentField[i]) {
+      dependentField[i][dependentAttribute] = dependentArray[i];
+    }
+  }
+  selectedField.addEventListener('change', function () {
+    dependentField[dependentAttribute] = selectedField.value;
+  });
+};
