d4537f980467ab978aef3ab931997d1ea6f45bc1
Elena Esina <hellemess@yandex.ru>
2017-02-24 23:24:46 +0300

    убрала недочеты

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,10 +3,10 @@
 window.synchronizeFields = (function () {
   return function (selectedField, dependentField, selectedArray, dependentArray, cb) {
     for (var i = 0; i < Math.max(selectedArray.length, dependentArray.length); i++) {
-      if (!!selectedField[i]) {
+      if (selectedField[i].tagName.toLowerCase() === 'option') {
         selectedField[i].value = selectedArray[i];
       }
-      if (!!dependentField[i]) {
+      if (dependentField[i].tagName.toLowerCase() === 'option') {
         dependentField[i].value = dependentArray[i];
       }
     }
